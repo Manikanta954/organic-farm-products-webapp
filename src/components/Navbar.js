d956d099fe3771bca73a0741cf9f65a2005cfe5c
Manikanta954 <talagamamanikanta98@gmail.com>
2024-07-18 20:16:28 +0530

    added notif badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const { cartItems } = useContext(CartContext);
+  const cartCount = cartItems.length;
+
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Organic Eggs</div>
+      <div className="navbar-brand">
+        <Link to="/" style={{textDecoration:"none"}}>Organic Eggs</Link>
+      </div>
       <div className="navbar-links">
         <Link to="/">Home</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/about-us">About Us</Link>
+        <div className="cart-link">
+          <Link to="/cart">Cart</Link>
+          {cartCount > 0 && <span className="cart-badge">{cartCount}</span>}
+        </div>
+        <Link to="/about">About Us</Link>
         <Link to="/profile">Profile</Link>
+        
       </div>
     </nav>
   );
