4014f93109b0b5afad6b51a1fbf62d6a7a5df093
Manikanta954 <talagamamanikanta98@gmail.com>
2024-07-21 23:15:10 +0530

    .

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -7,15 +7,15 @@ function AboutUs() {
     <div className="about-us">
       <h1>About Us</h1>
       <p className="about-description" >
-        Welcome to Organic Eggs! We are dedicated to providing you with the freshest and
-        highest quality organic eggs from free-range chickens. Our farm follows the best
+        Welcome to Organic farm! We are dedicated to providing you with the freshest and
+        highest quality organic meat and eggs from free-range chickens. Our farm follows the best
         practices to ensure the well-being of our chickens and the sustainability of our
         environment.
       </p>
       <p className="about-description">
-        Our mission is to deliver healthy and delicious eggs to your doorstep while
+        Our mission is to deliver healthy and delicious meat and eggs to your doorstep while
         promoting a sustainable and ethical approach to farming. Thank you for choosing
-        Organic Eggs.
+        Organic farm.
       </p>
       <div className="founder-section">
         <img src={CEO} alt="Founder" className="founder-pic" />
